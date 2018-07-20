ab1eadc10065bb142f7a37d193735c2b60981167
unknown <shivaramv23@gmail.com>
2018-07-20 15:31:04 +0530

    SQL file with queries for koans

--- src/koans/basics.sql
+++ src/koans/basics.sql
@@ -2,22 +2,22 @@
 SELECT 1
 
 -- Meditate on lower-case queries
-_____ 1
+select 1
 
 -- Meditate on selecting all columns from a table
 select * from book
 
 -- Meditate on selecting one column (title) from a table
-select _____ from book
+select title from book
 
 -- Meditate on selecting some columns (title, available) from a table
-select _____, _____ from book
+select title, available from book
 
 -- Meditate on adding a new book
 insert into book(title, genre, year_published, available)
-values('_____', '_____', _____, 1)
+values('1984', 'Dystopian Novel', 1949, 1)
 
 -- Meditate on adding two new books
 insert into book(title, genre, year_published, available)
-values('_____', '_____', _____, 1)
-,('_____', '_____', _____, 1)
+values('Animal Farm', 'Novel', 1945, 1)
+,('Macbeth', 'Drama', 1606, 1)
